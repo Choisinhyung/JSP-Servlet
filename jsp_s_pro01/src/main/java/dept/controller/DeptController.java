d8e5db99d9794e7cf111ab7f86beef60239fa3bd
최신형 <tlsgudrhfem@naver.com>
2022-07-03 23:32:29 +0900

    MVC 모델 완성

--- jsp_s_pro01/src/main/java/dept/controller/DeptController.java
+++ jsp_s_pro01/src/main/java/dept/controller/DeptController.java
@@ -19,6 +19,12 @@ public class DeptController extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		DeptService service = new DeptService();
 		List<DeptDTO> deptDatas = service.getAll();
+		
+		request.setAttribute("deptDatas", deptDatas);   // 컨트롤러에서 jsp파일로 값을 가져오는 코드
+		// request 객체에 속성을 설정한다고 보면 됨.
+		
+		String view = "/WEB-INF/jsp/dept/index.jsp";
+		request.getRequestDispatcher(view).forward(request, response); // request가 같이 전달
 	}
 
 }
